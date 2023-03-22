dd8fab32075086105b4f621693bc6e6f85bfd79e
Lee Devine <94686562+LeeaDevine@users.noreply.github.com>
2023-03-22 16:13:22 +0000

    Updated UnsortedArray -working
    working version 1.0.
    
    testing still needed.
    revisit.

--- src/queuemanager/UnsortedArrayPriorityQueue.java
+++ src/queuemanager/UnsortedArrayPriorityQueue.java
@@ -19,10 +19,6 @@ public class UnsortedArrayPriorityQueue<T> implements PriorityQueue<T> {
      */
     private final Object[] storage;
 
-    /**
-     * The size of the storage array.
-     */
-    private final int capacity;
 
     /**
      * The index of the last item stored.
@@ -38,7 +34,6 @@ public class UnsortedArrayPriorityQueue<T> implements PriorityQueue<T> {
      */
     public UnsortedArrayPriorityQueue(int size) {
         storage = new Object[size];
-        capacity = size;
         tailIndex = -1;
     }
     
@@ -46,7 +41,7 @@ public class UnsortedArrayPriorityQueue<T> implements PriorityQueue<T> {
      * Helper Method
      * Find the highest priority item from the array
      * 
-     * @return headPosition
+     * @return headPosition -> largest priority value
      * @throws QueueUnderflowException 
      */
     private int getHighestPriorityItem() throws QueueUnderflowException{
@@ -56,20 +51,32 @@ public class UnsortedArrayPriorityQueue<T> implements PriorityQueue<T> {
         }
         
         /**
-         * loop through array and find largest priority integer
-         * set this as new highest priority.
+         * create head pointer
+         * loop through array and find largest priority value
          */
         int headPosition = tailIndex;
-        for(int i = tailIndex; i >= 0; i--){
+        for(int i = tailIndex; i >= 0; --i){
+        
+            /**
+             * Create 2 integer variables to keep track of current highest priority
+             * and what the next priority index is.
+             */
+            int currentPriority = ((PriorityItem<T>)storage[headPosition]).getPriority();
+            int nextPriority = ((PriorityItem<T>)storage[i]).getPriority();
             
+            /**
+             * compare current greater than next
+             * set new highest priority / else keep looping
+             */
+            headPosition = (currentPriority > nextPriority) ? headPosition : i;
         }
         return headPosition;
     }
     
     /**
-     * display head of array
+     * display highest priority item in unsorted array
      * 
-     * @return
+     * @return index of item that has highest priority using helper method -> getHighestPriorityItem()
      * @throws QueueUnderflowException 
      */
     @Override
@@ -77,37 +84,53 @@ public class UnsortedArrayPriorityQueue<T> implements PriorityQueue<T> {
         if (isEmpty()) {
             throw new QueueUnderflowException();
         } else {
-            return ((PriorityItem<T>) storage[0]).getItem();
+            /**
+             * use helper method -> getHighestPriorityItem
+             * @return highest priority item
+             */
+            int headPosition = getHighestPriorityItem();
+            return ((PriorityItem<T>)storage[headPosition]).getItem();
         }
     }
 
+    /**
+     * add new item to end of list
+     * as array is unsorted -> we do not need to do anything else
+     * 
+     * @param item
+     * @param priority
+     * @throws QueueOverflowException 
+     */
     @Override
     public void add(T item, int priority) throws QueueOverflowException {
-        tailIndex = tailIndex + 1;
-        if (tailIndex >= capacity) {
-            /* No resizing implemented, but that would be a good enhancement. */
-            tailIndex = tailIndex - 1;
+        
+        if(tailIndex >= storage.length -1){
             throw new QueueOverflowException();
-        } else {
-            /* Scan backwards looking for insertion point */
-            int i = tailIndex;
-            while (i > 0 && ((PriorityItem<T>) storage[i - 1]).getPriority() < priority) {
-                storage[i] = storage[i - 1];
-                i = i - 1;
-            }
-            storage[i] = new PriorityItem<>(item, priority);
+        } else{
+            tailIndex++;
+            storage[tailIndex] = new PriorityItem<>(item, priority);
         }
     }
 
+    /**
+     * 
+     * @throws QueueUnderflowException 
+     */
     @Override
     public void remove() throws QueueUnderflowException {
         if (isEmpty()) {
             throw new QueueUnderflowException();
         } else {
-            for (int i = 0; i < tailIndex; i++) {
-                storage[i] = storage[i + 1];
-            }
-            tailIndex = tailIndex - 1;
+            
+            /**
+             * user helper method -> getHighestPriorityItem
+             * remove headPosition = highest priority 
+             * decrement array by 1
+             */
+            int headPosition = getHighestPriorityItem();
+            storage[headPosition] = storage[tailIndex];
+            
+            tailIndex--;
         }
     }
 
@@ -119,7 +142,7 @@ public class UnsortedArrayPriorityQueue<T> implements PriorityQueue<T> {
     @Override
     public String toString() {
         String result = "[";
-        for (int i = 0; i <= tailIndex; i++) {
+        for (int i = 0; i <= tailIndex; ++i) {
             if (i > 0) {
                 result = result + ", ";
             }
