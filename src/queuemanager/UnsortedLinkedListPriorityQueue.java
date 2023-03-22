1561f6659b780f376efcc5c941d76d9169749b56
Lee Devine <94686562+LeeaDevine@users.noreply.github.com>
2023-03-22 22:17:50 +0000

    Update UnsortedLinkedListPriorityQueue.java
    added abstract methods.
    copy code from sorted LL and continue

--- src/queuemanager/UnsortedLinkedListPriorityQueue.java
+++ src/queuemanager/UnsortedLinkedListPriorityQueue.java
@@ -4,8 +4,37 @@ package queuemanager;
 /**
  * Implementation of the PriorityQueue ADT using an unsorted linked list for storage.
  * 
+ * @references
+ * Buchalka, T. (2021). Complete Java Software Developer Masterclass (for Java 10). [online] Udemy. Available at: https://www.udemy.com/course/java-the-complete-java-developer-course/.
+ * Bro Code (2021). Data Structures and Algorithms Full Course 【𝙁𝙧𝙚𝙚】. YouTube. Available at: https://www.youtube.com/watch?v=CBYHwZcbD-s [Accessed 21 Jul. 2022].
+ * FreeCodeCamp (2019). Data Structures Easy to Advanced Course - Full Tutorial from a Google Engineer. [online] www.youtube.com. Available at: https://www.youtube.com/watch?v=RBSGKlAvoiM.
+ * 0612 TV w/ NERDfirst (2017). Node-Based Data Structures in Java. [online] www.youtube.com. Available at: https://www.youtube.com/watch?v=PEdFSWJm0TA&t=446s [Accessed 21 Mar. 2023].
+ * Amit-L (2018). Priority queue using singly linked-list. [online] Gist. Available at: https://gist.github.com/Amit-L/2753719e10c0a4e3d0040fae5d8c975c [Accessed 21 Mar. 2023].
+ * 
+ * @param <T> The type of things being stored.
+ * 
  * @author Lee Devine 
  */
-public class UnsortedLinkedListPriorityQueue {
+public class UnsortedLinkedListPriorityQueue<T> implements PriorityQueue<T>{
+
+    @Override
+    public void add(T item, int priority) throws QueueOverflowException {
+        
+    }
+
+    @Override
+    public T head() throws QueueUnderflowException {
+       
+    }
+
+    @Override
+    public void remove() throws QueueUnderflowException {
+        
+    }
+
+    @Override
+    public boolean isEmpty() {
+        
+    }
     
 }
