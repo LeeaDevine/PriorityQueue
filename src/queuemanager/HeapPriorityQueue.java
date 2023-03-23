3dd0c3fc0b0fc7f1641e064e3e8b26e056c7c7f4
Lee Devine <94686562+LeeaDevine@users.noreply.github.com>
2023-03-23 12:51:48 +0000

    Update HeapPriorityQueue.java
    added main references

--- src/queuemanager/HeapPriorityQueue.java
+++ src/queuemanager/HeapPriorityQueue.java
@@ -5,6 +5,10 @@ package queuemanager;
  * Implementation of the PriorityQueue ADT using a sorted array for storage.
  * 
  * @references
+ * Abdui Bari (2019). 2.6.3 Heap - Heap Sort - Heapify - Priority Queue - Youtube. [online] www.youtube.com. Available at: https://www.youtube.com/watch?v=HqPJF2L5h9U.
+ * Buchalka, T. (2021). Complete Java Software Developer Masterclass (for Java 10). [online] Udemy. Available at: https://www.udemy.com/course/java-the-complete-java-developer-course/.
+ * Bro Code (2021). Data Structures and Algorithms Full Course 【𝙁𝙧𝙚𝙚】. YouTube. Available at: https://www.youtube.com/watch?v=CBYHwZcbD-s [Accessed 21 Jul. 2022].
+ * FreeCodeCamp (2019). Data Structures Easy to Advanced Course - Full Tutorial from a Google Engineer. [online] www.youtube.com. Available at: https://www.youtube.com/watch?v=RBSGKlAvoiM.
  * 
  * 
  * @param <T>
