5176269cbbe10b18c0c0bb3a09f3765dcd98af20
Lee Devine <94686562+LeeaDevine@users.noreply.github.com>
2023-03-23 17:53:51 +0000

    Create SortedArrayPriorityQueueTest.java

---
+++ test/queuemanager/SortedArrayPriorityQueueTest.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package queuemanager;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Leead
+ */
+public class SortedArrayPriorityQueueTest {
+    
+    public SortedArrayPriorityQueueTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of head method, of class SortedArrayPriorityQueue.
+     */
+    @Test
+    public void testHead() throws Exception {
+        System.out.println("head");
+        SortedArrayPriorityQueue instance = null;
+        Object expResult = null;
+        Object result = instance.head();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of add method, of class SortedArrayPriorityQueue.
+     */
+    @Test
+    public void testAdd() throws Exception {
+        System.out.println("add");
+        Object item = null;
+        int priority = 0;
+        SortedArrayPriorityQueue instance = null;
+        instance.add(item, priority);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of remove method, of class SortedArrayPriorityQueue.
+     */
+    @Test
+    public void testRemove() throws Exception {
+        System.out.println("remove");
+        SortedArrayPriorityQueue instance = null;
+        instance.remove();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isEmpty method, of class SortedArrayPriorityQueue.
+     */
+    @Test
+    public void testIsEmpty() {
+        System.out.println("isEmpty");
+        SortedArrayPriorityQueue instance = null;
+        boolean expResult = false;
+        boolean result = instance.isEmpty();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toString method, of class SortedArrayPriorityQueue.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        SortedArrayPriorityQueue instance = null;
+        String expResult = "";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
